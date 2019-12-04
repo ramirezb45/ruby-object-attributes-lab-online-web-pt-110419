e5cb5c32835cae856d9ea7c8a53f6e9e9a203bdf
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-12-04 18:26:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,18 @@
-  class Dog 
-    def initialize(breed)
-      @breed = breed
-    end
-    
-    def breed 
-      @breed
-    end
+class Dog
+  def name
+    @name
   end
-  
-  
-    
-      
+
+  def name=(name)
+    @name = name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+end
   
